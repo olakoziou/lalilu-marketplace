59a35cc8fd59e5252ebabb10ccca28c2f9a4f471
Ola Kozioł <o.stanislawska92@gmail.com>
2021-03-13 16:14:13 +0100

    initial configs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, ratione quo! Nobis similique doloremque a atque, ipsum nam deserunt iure amet illo ex placeat. Corrupti asperiores quae voluptatibus aperiam ipsum id voluptates sequi nam deleniti assumenda, eligendi cumque tempora perferendis similique pariatur vero voluptate, suscipit consequatur? Saepe magnam ipsa dolorem.</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
